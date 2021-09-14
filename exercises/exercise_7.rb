8c0e63cfebe1c5ac87dd33bee68595c210d8c89f
Michael <michael.dburke56@gmail.com>
2021-09-13 20:59:57 -0400

    Ex.7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,27 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40 }
+  validates :hourly_rate, numericality: { less_than: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+end
+
+def add_store
+  puts "\nName of new store: "
+  new_store_name = gets.chomp
+  new_store = Store.create(name: new_store_name)
+  new_store.valid?
+  new_store.errors.messages.each do |error|
+    puts error
+  end  
+end  
+
+add_store
