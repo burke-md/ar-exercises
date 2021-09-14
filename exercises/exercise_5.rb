b650bb6114775deaf2982bb6c169b0371142b0c0
Michael <michael.dburke56@gmail.com>
2021-09-13 20:12:03 -0400

    Ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "\nREVENUE"
+total_revenue = Store.sum("annual_revenue")
+puts total_revenue
+
+puts "\nREVENUE FOR ALL STORES"
+puts total_revenue / Store.all.count 
+
+puts "\nNUMBER OF STORES > 1M$ / YEAR"
+puts Store.where("annual_revenue > 1000000").count
